9067f29fa85d4c7a8395726ed13c342b3604db7d
Alex Rothenberg <alex@alexrothenberg.com>
2020-09-01 16:05:37 -0400

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ which git
 echo `pwd`
 echo `ls`
 
-for f in **/*.dot
-do
-  dot -Tsvg $f -o $f.svg
-done
+# for f in **/*.dot
+# do
+#   dot -Tsvg $f -o $f.svg
+# done
 
 sh -c "git config --global user.name '${GITHUB_ACTOR}' \
       && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com' \
