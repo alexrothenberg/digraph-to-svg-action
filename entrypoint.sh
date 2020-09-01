a820220c1c3e16bbaa4910b5c4db1acf33c52597
Alex Rothenberg <alex@alexrothenberg.com>
2020-09-01 16:26:10 -0400

    docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 set -exv
 
+echo "In the digraph-to-svg action"
 
-echo "hi"
-which git
-echo "hi2"
-echo `pwd`
-echo "hi3"
-echo `ls`
-echo "hi4"
-
-# for f in **/*.dot
-# do
-#   dot -Tsvg $f -o $f.svg
-# done
+for f in **/*.dot
+do
+  echo $f
+  dot -Tsvg $f -o $f.svg
+done
 
 sh -c "git config --global user.name '${GITHUB_ACTOR}' \
       && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com' \
